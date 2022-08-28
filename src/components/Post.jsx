89183fc4a62a757095aacc12e47e39e7d2b6b32b
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-27 19:32:18 -0700

    updated Post.jsx and am successfully uploading images to cloudinary through front-end interface

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,20 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { insertImage } from '../../state/services/fetch-utils.js';
 
 export default function Post() {
-  // const [image, setImage] = useState('');
+  const [fileInputState, setFileInputState] = useState('');
+  const [previewSource, setPreviewSource] = useState('');
+  const [selectedFile, setSelectedFile] = useState('');
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    previewFile(file);
+  };
 
-  const handleSubmit = async (image) => {
-    await insertImage(image);
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
+    };
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!previewSource) return;
+    uploadImage(previewSource);
+  };
+
+  const uploadImage = async (image) => {
+    console.log('image', image);
+    try {
+      await insertImage(image);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <h1>upload pic here</h1>
-        <input type="file"></input>
+        <input 
+          type="file" 
+          name="image" 
+          onChange={handleFileInputChange}
+          value={fileInputState}>
+        </input>
+
         <button type="submit">upload!</button>
       </form>
+      {previewSource && (
+        <img src={previewSource} alt="image-preview" />
+      )}
       Post
     </div>
   );
