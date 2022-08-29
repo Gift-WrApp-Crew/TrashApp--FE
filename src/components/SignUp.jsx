2fcf8fa8d8e9d042b6451377fe76530f287d8ecf
JeffAllison <itsjallison@gmail.com>
2022-08-29 11:06:30 -0700

    created signIn and signUp jsx files to log in and signUp user, signUp includes username prompt

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { signUpUserFunction } from './services/fetch-utils';
+export default function SignUp({ setCurrentUser }) {
+  const [signUpUser, setSignUpUser] = useState({ email: '', password: '', username: '' });
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    await signUpUserFunction(signUpUser);
+    await signInUserFunction(signUpUser);
+    const user = await getUser();
+    setCurrentUser(user);
+  }
+  return (
+    <div>
+      <div className="sign-up">
+        <form onSubmit={handleSignUp}>
+          <h4>Sign Up</h4>
+          <label>
+            set username
+            <input
+              className="username-input"
+              value={signUpUser.username}
+              onChange={(e) => setSignUpUser({ ...signUpUser, username: e.target.value })}
+            />
+          </label>
+          <label>
+            email
+            <input
+              className="sign-up-input"
+              value={signUpUser.email}
+              onChange={(e) => setSignUpUser({ ...signUpUser, email: e.target.value })}
+            />
+          </label>
+          <label>
+            password
+            <input
+              className="sign-up-input"
+              value={signUpUser.password}
+              type="password"
+              onChange={(e) => setSignUpUser({ ...signUpUser, password: e.target.value })}
+            />
+          </label>
+          <button className="sign-up-button">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+}
